b18af8b47bc524011403a2617b0e158b5d2cdff2
nayaraogioni <159180940+nayaraogioni@users.noreply.github.com>
2025-03-04 11:49:22 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nogioni- <nogioni-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 18:50:00 by nogioni-          #+#    #+#             */
+/*   Updated: 2025/01/29 19:18:13 by nogioni-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int ac, char **av)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+	
+	a = NULL;
+	b = NULL;
+	if (ac == 2 || (ac == 2 && !av[1][0]))
+		return (1);
+	else if (ac == 2)
+		av = ft_split(av[1], ' ');
+	init_stack(&a, av + 1)
+	if (!sorted_stack(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a, false);
+		else if (stack_len(a) = 3)
+			sort_three(&a);
+		else
+			sort_stacks(&a, &b);
+	}
+	free_stack(&a);
+}
